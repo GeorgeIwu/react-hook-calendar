c1ff0098eae123923777d3e42ae8642da0f49011
George Iwu <c.georgeiwu@gmail.com>
2020-03-16 06:38:10 -0700

    add weeks

--- src/calendar/month/CellContent.js
+++ src/calendar/month/CellContent.js
@@ -2,40 +2,35 @@ import React from 'react';
 import * as dateFns from 'date-fns';
 
 const CellContent = ({ setDate, date }) => {
-  const dateFormat = 'd';
-  const today = new Date();
-  const weekDaysArray = Array.from(Array(7), (_, i) => i);
-  const monthWeeks = dateFns.getWeeksInMonth(date, {weekStartsOn: 1});
-  const monthStart = dateFns.startOfMonth(date);
-  const weekStart = dateFns.startOfWeek(monthStart, {weekStartsOn: 1});
-  const monthWeeksArray = Array.from(Array(monthWeeks), (_, i) => i);
+  const currentDate = new Date();
+  const weekDays = Array.from(Array(7), (_, i) => i);
+  const monthWeeks = Array.from(Array(dateFns.getWeeksInMonth(date, {weekStartsOn: 1})), (_, i) => i) ;
+  const monthStartWeek = dateFns.startOfWeek(dateFns.startOfMonth(date), {weekStartsOn: 1});
+  const monthWeeksFromMonth = monthWeeks.map(monthWeek => dateFns.addWeeks(monthStartWeek, monthWeek));
 
   const onClickCell = data => {
     setDate(dateFns.parse(data));
   };
 
-  const rowZ = monthWeeksArray.map((monthWeek, i) => {
-    const weekDaysForMonthWeek = weekDaysArray.map(
-      weekDay => weekDay + monthWeek * 7
-    );
+  const rowZ = monthWeeksFromMonth.map((monthWeekDate, i) => {
+    const weekDatesFromWeek = weekDays.map(weekDay => dateFns.addDays(monthWeekDate, weekDay));
 
     return (
       <div className="row" key={i}>
-        {weekDaysForMonthWeek.map((item, index) => {
-          const day = dateFns.addDays(weekStart, item);
-          const formattedDate = dateFns.format(day, dateFormat);
+        {weekDatesFromWeek.map((weekDate, index) => {
+          const formattedDate = dateFns.format(weekDate, 'd');
 
           return (
             <div
               className={`col cell ${
-                !dateFns.isSameMonth(day, date)
+                !dateFns.isSameMonth(weekDate, date)
                   ? 'disabled'
-                  : dateFns.isSameDay(day, today)
+                  : dateFns.isSameDay(weekDate, currentDate)
                   ? 'selected'
                   : ''
               }`}
               key={index}
-              onClick={() => onClickCell(day)}
+              onClick={() => onClickCell(weekDate)}
             >
               <span className="number">{formattedDate}</span>
               <span className="bg">{formattedDate}</span>
